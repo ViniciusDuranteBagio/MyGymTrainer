a095fbc4d1e871d7dfd1a48f8c22b0f4c2248c72
ViniciusDuranteBagio <viniciusduranteb@gmail.com>
2023-06-01 17:44:12 -0300

    COMITANDO TRY CATCH PARA VER O ERRO

--- routes/web.php
+++ routes/web.php
@@ -14,44 +14,49 @@ use \App\Http\Controllers\Auth\LogoutController;
 | contains the "web" middleware group. Now create something great!
 |
 */
+try {
+    Auth::routes();
 
-Auth::routes();
-
-Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('index');
+    Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('index');
 
 
-Route::get('/ranking', [\App\Http\Controllers\UsersController::class, 'ranking'])->name("ranking");
+    Route::get('/ranking', [\App\Http\Controllers\UsersController::class, 'ranking'])->name("ranking");
 
-Route::get('/login', function(){
-    return view('login');
-})->name("login");
+    Route::get('/login', function(){
+        return view('login');
+    })->name("login");
 
-Route::get('/register', function(){
-    return view('register');
-})->name("register");
+    Route::get('/register', function(){
+        return view('register');
+    })->name("register");
 
-Route::get('/treino-em-andamento/{id}', [\App\Http\Controllers\WorkoutController::class, "inProgressWorkout"])->name("inProgressWorkout");
+    Route::get('/treino-em-andamento/{id}', [\App\Http\Controllers\WorkoutController::class, "inProgressWorkout"])->name("inProgressWorkout");
 
-Route::get('/treino-finalizado/{id}', [\App\Http\Controllers\WorkoutController::class, "workoutCompleted"])->name("workoutCompleted");
+    Route::get('/treino-finalizado/{id}', [\App\Http\Controllers\WorkoutController::class, "workoutCompleted"])->name("workoutCompleted");
 
-Route::get('/treino-iniciar/{id}', [\App\Http\Controllers\WorkoutController::class, "startWorkout"])->name("workout");
+    Route::get('/treino-iniciar/{id}', [\App\Http\Controllers\WorkoutController::class, "startWorkout"])->name("workout");
 
-Route::get('/minha-conta/{id}', [\App\Http\Controllers\UsersController::class, 'myAccount'])->name("myAccount");
+    Route::get('/minha-conta/{id}', [\App\Http\Controllers\UsersController::class, 'myAccount'])->name("myAccount");
 
-Route::get('/editar-minha-conta/{id}', [\App\Http\Controllers\UsersController::class, 'editMyAcount'])->name("editAccount");
+    Route::get('/editar-minha-conta/{id}', [\App\Http\Controllers\UsersController::class, 'editMyAcount'])->name("editAccount");
 
-Route::get('/trocar-senha', function(){
-    return view('editPassword');
-})->name("editPassword");
+    Route::get('/trocar-senha', function(){
+        return view('editPassword');
+    })->name("editPassword");
 
-Route::get('/troca-treino/{id}',[\App\Http\Controllers\UsersController::class, "changeWorkout"])->name("changeWorkout");
+    Route::get('/troca-treino/{id}',[\App\Http\Controllers\UsersController::class, "changeWorkout"])->name("changeWorkout");
 
-Route::get('/vencimento/{id}', [\App\Http\Controllers\UsersController::class, "contract"])->name("expireDate");
+    Route::get('/vencimento/{id}', [\App\Http\Controllers\UsersController::class, "contract"])->name("expireDate");
 
-Route::group(['middleware' => ['auth']], function() {
+    Route::group(['middleware' => ['auth']], function() {
 
-    Route::get('/logout', [LogoutController::class, 'perform'])->name('logout');
-});
+        Route::get('/logout', [LogoutController::class, 'perform'])->name('logout');
+    });
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+}catch (Throwable $exception){
+    Kint::$depth_limit = 3;
+    Kint::dump($exception);
+}
 
