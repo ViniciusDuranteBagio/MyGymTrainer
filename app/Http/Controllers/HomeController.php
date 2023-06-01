59353c1b358716f133585979ba4ed9eba902f724
ViniciusDuranteBagio <viniciusduranteb@gmail.com>
2023-06-01 18:31:27 -0300

    retirando exit

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,7 +24,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        exit('a');
         //todo criar um get user logado com isso retornando o objeto do usuario
 //        if (! $this->userHasCompleteInformation()) {
             //fazer um select de experiencia em academia -> vai setar um treino padrão para o usuario
