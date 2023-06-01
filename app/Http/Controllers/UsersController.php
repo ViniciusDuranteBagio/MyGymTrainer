6167002c37c76df2d24e6f1b9d68b333c7b138ac
ViniciusDuranteBagio <viniciusduranteb@gmail.com>
2023-06-01 18:41:47 -0300

    teste

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -140,10 +140,10 @@ class UsersController extends Controller
     public function ranking()
     {
         $params = [];
-        $usuarios = User::all();
-        $users = $this->getDataForRanking($usuarios->sortBy("nr_score"));
-        $params['usuarios'] = $users;
-        $params['rakingGeral']= $usuarios->sortBy("nr_score",4, true);
+//        $usuarios = User::all();
+//        $users = $this->getDataForRanking($usuarios->sortBy("nr_score"));
+        $params['usuarios'] = [];
+        $params['rakingGeral']= [];
         return view('ranking', $params);
     }
 
